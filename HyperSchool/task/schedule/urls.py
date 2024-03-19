49a6d6dfe5610dc394206ad8c49f1b99da14c910
CapitalAlive <120092274+CapitalAlive@users.noreply.github.com>
2024-03-19 09:54:32 +0900

    updating the paths

--- HyperSchool/task/schedule/urls.py
+++ HyperSchool/task/schedule/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path("", lambda request: redirect("main"), name="index"),
     path("schedule/main/", views.main, name="main"),
     path("schedule/course_details/<pk>", views.course_details, name="courses_url"),
     path("schedule/teacher_details/<pk>", views.teacher_details, name="teacher_url"),
